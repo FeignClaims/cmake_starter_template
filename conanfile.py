01ea1f4ebb661a718d526a0b01f43afb56cf1462
FeignClaims <www4112379@163.com>
2023-03-18 18:15:14 +0800

    Reorder conanfile.py functions to make it top-down

--- conanfile.py
+++ conanfile.py
@@ -5,6 +5,15 @@ from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps
 class starterRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
+    def requirements(self):
+        self.requires("fmt/[>=9.1.0]")
+        self.requires("ms-gsl/[>=4.0.0]")
+        self.requires("range-v3/[>=0.12.0]")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.25]")
+        self.test_requires("gtest/[>=1.13.0]")
+
     def layout(self):
         self.folders.build_folder_vars = ["settings.compiler"]
         cmake_layout(self)
@@ -14,12 +23,3 @@ class starterRecipe(ConanFile):
         toolchain = CMakeToolchain(self)
         toolchain.presets_prefix = ""
         toolchain.generate()
-
-    def requirements(self):
-        self.requires("fmt/[>=9.1.0]")
-        self.requires("ms-gsl/[>=4.0.0]")
-        self.requires("range-v3/[>=0.12.0]")
-
-    def build_requirements(self):
-        self.tool_requires("cmake/[>=3.25]")
-        self.test_requires("gtest/[>=1.13.0]")
