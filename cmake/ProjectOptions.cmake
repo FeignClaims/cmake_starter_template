c6f48c27c1cc437bbe30c5e3a7d88ab9fbcba52b
FeignClaims <www4112379@163.com>
2023-03-31 18:12:53 +0800

    Revert "Switch to use my project_options fork before merge"
    This reverts commit 06d242ba6a31110f9fe89094c13649a0dba8b3c7.

--- cmake/ProjectOptions.cmake
+++ cmake/ProjectOptions.cmake
@@ -1,12 +1,12 @@
 include_guard()
 
-message(STATUS "Downloading ProjectOptions from https://github.com/FeignClaims/project_options.git\n"
+message(STATUS "Downloading ProjectOptions from https://github.com/aminya/project_options.git\n"
   "Please make sure that you can connect to github"
 )
 include(FetchContent)
 FetchContent_Declare(_project_options
-  GIT_REPOSITORY https://github.com/FeignClaims/project_options.git
-  GIT_TAG feature/package_project_extension
+  GIT_REPOSITORY https://github.com/aminya/project_options.git
+  GIT_TAG main
   GIT_SHALLOW true
 )
 FetchContent_MakeAvailable(_project_options)
